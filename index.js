13d512386597e87b001f0728015c7b8c9364762a
donbogo <donbogo@hotmail.com>
2021-03-01 00:47:38 -0500

    Update index.js
    Documentación

--- index.js
+++ index.js
@@ -3,14 +3,15 @@
 const app = require('./app');
 
 /**
- * 
+ * Sabrás si un humano es mutante, si encuentras más de una secuencia de cuatro letras iguales, 
+ * de forma oblicua, horizontal o vertical.
  * @param {*} req 
  * @param {*} res 
  */
 module.exports.esMutante = async (req, res) => {
     try {
-        let dna = req.body.adn;
-        let mutante = await app.isMutant(dna.toString());
+        let dna = req.body.adn; // body de la petición
+        let mutante = await app.isMutant(dna.toString()); // LLamado función validar adn
         return res.status(mutante ? 200 : 403).send({ codigo: mutante ? 200 : 403, mensaje: mutante ? 'OK' : 'Forbidden' });
     } catch (error) {
         console.error(error);
